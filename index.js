2410ce388ad0c65763811a9cc31559dcc1b33663
ren <renzhiwei@lewell.cn>
2019-02-28 15:46:52 +0800

    show the node generator qrcode in web

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const WebSocket = require('ws');
-
+var qrcode = require('qrcode-terminal');
 const wss = new WebSocket.Server({ port: 8080 });
 
-wss.on('connection', function connection(ws) {
-  ws.send('something');
-  ws.send('something'+Math.floor(Math.random()*100));
-});
+console.log(wss.on('connection', function connection(ws) {
+  // ws.send('something');
+  // ws.send('something'+Math.floor(Math.random()*100));
+  qrcode.generate('http://github.com', {small: true},  function (qrcode) {
+    ws.send((qrcode))
+    console.log(qrcode)
+  })
+  
+}))
